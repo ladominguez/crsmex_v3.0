089f4c9509da3cc020edc39509ccd5399b4a6f7c
ladominguez <ladominguez@ucla.edu>
2022-11-13 22:42:00 -0600

    Loading data in memory added.

--- find_repeaters_v3.py
+++ find_repeaters_v3.py
@@ -1,18 +1,25 @@
 import os
+import time
 from obspy.core import read
 from datetime import date
 from obspy.geodetics.base import gps2dist_azimuth
 import json
 import utils
+import argparse
 
 config = utils.load_configuration()
 
 today         = date.today()
 start_time    = time.time()
 
-path_data = os.path.join(config['root'],'sac')
+root = config['root']
 
 if __name__ == '__main__':
-    print("Loading data in memory from " + path_data)
-    master    = read(os.path.join(path_data,'sac','*HZ*.sac'))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--station', type=str, help = 'Station id.') 
+    args = parser.parse_args()
+    station = args.station
+
+    print("Loading data in memory for station:  " + station)
+    master = read(os.path.join(root, station.upper(), 'sac', '*HZ*.sac'))
     print("Number of waveforms = "+ str(len(master)))
--- utils.py
+++ utils.py
@@ -2,6 +2,5 @@ import os
 import json
 
 def load_configuration():
-    root_crsmex = os.environ["ROOT_CRSMEX_V3"]
-    config_file = open(os.path.join(root_crsmex, 'config.json'))
+    config_file = open('./config.json')
     return json.load(config_file)
