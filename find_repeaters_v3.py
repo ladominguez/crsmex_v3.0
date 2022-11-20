fb2c162c92aca969e9c3fd9df322e17507ea8f7a
Luis Antonio Dominguez <ladominguez@ucla.edu>
2022-11-20 16:14:54 -0600

    Converting processing into a function. Right before using Numba

--- find_repeaters_v3.py
+++ find_repeaters_v3.py
@@ -13,6 +13,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import crsmex as crs
+from numba import jit
 
 config = utils.load_configuration()
 today         = date.today()
@@ -21,6 +22,54 @@ start_time    = time.time()
 root = config['root']
 
 plotting = False
+
+#@jit(nopython=True)
+def processing(waveforms, N, plotting = False):
+    output_list = []
+
+    for k in tqdm(range(0,N)):
+        kevnm_master = waveforms[k].stats.sac.kevnm.rstrip()
+        ds_master = waveforms[k].stats.sampling_rate
+        t5_master = waveforms[k].stats.sac.t5 - 1.5
+        id_master = np.searchsorted(waveforms[k].times(
+                reftime=waveforms[k].stats.starttime - waveforms[k].stats.sac.b), 
+                t5_master, side="left")
+        master = waveforms[k].data[id_master:id_master+config['Win']]
+        datetime_master = datetime.datetime.strftime(waveforms[k].stats.starttime.datetime,'%Y.%j.%H%M%S')
+        for n in range(k+1,N):
+            t5_test = waveforms[n].stats.sac.t5 - 1.5
+            ds_test = waveforms[k].stats.sampling_rate
+            id_test = np.searchsorted(waveforms[n].times(
+                                      reftime=waveforms[n].stats.starttime - waveforms[n].stats.sac.b), 
+                                      t5_test, side="left")
+            test = waveforms[n].data[id_test:id_test+config['Win']] 
+            try:
+                CorrelationCoefficient, tshift = crs.get_correlation_coefficient(master, test, 1/ds_master)
+            except:
+                print('k: ', k, ' n: ', n)
+                exit()
+            
+            if CorrelationCoefficient >= config['Threshold']:
+                kevnm_test = waveforms[n].stats.sac.kevnm.rstrip()
+                coherence = crs.coherency(master, test, config['low_f'], config['high_f'], ds_master)
+                datetime_test = datetime.datetime.strftime(waveforms[n].stats.starttime.datetime,'%Y.%j.%H%M%S')
+                outline = {1 : datetime_master, 2 : datetime_test, 3 :
+                        int(round(CorrelationCoefficient*1e4)), 4 : int(round(coherence*1e4)), 
+                        5 : '%08d' % (int(kevnm_master)), 6: '%08d' % (int(kevnm_test))}
+                output_list.append(outline)
+            
+            #print('cc: ', round(CorrelationCoefficient*1e4), ' coh: ', round(coherence*1e4))
+            
+            if plotting:
+                fig, ax = plt.subplots(3,1, figsize=(22, 12))
+                t = np.linspace(0, (config['Win']-1)/ds_master, config['Win'])
+                ax[0].plot(t,master, color='black', linewidth=0.5)
+                ax[1].plot(t,test, color='black', linewidth=0.5)
+                ax[2].plot(t, crs.FFTshift(master/np.max(np.abs(master)),float(tshift*ds_master)), linewidth = 0.4)
+                ax[2].plot(t, crs.FFTshift(master/np.max(np.abs(master)),float(tshift*ds_master)), linewidth = 0.4)
+                fig.savefig('plot_' + str(k) + '_' + str(n) + '.png')
+                plt.close()
+    return output_list
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -48,45 +97,12 @@ if __name__ == '__main__':
             corners=config['n_poles'], 
             zerophase=True )
 
-    output_list = []
-    for k in tqdm(range(0,N)):
-        kevnm_master = waveforms[k].stats.sac.kevnm.rstrip()
-        ds_master = waveforms[k].stats.sampling_rate
-        t5_master = waveforms[k].stats.sac.t5 - 1.5
-        id_master = np.searchsorted(waveforms[k].times(
-                reftime=waveforms[k].stats.starttime - waveforms[k].stats.sac.b), 
-                t5_master, side="left")
-        master = waveforms[k].data[id_master:id_master+config['Win']]
-        datetime_master = datetime.datetime.strftime(waveforms[k].stats.starttime.datetime,'%Y.%j.%H%M%S')
-        for n in range(k+1,N):
-            t5_test = waveforms[n].stats.sac.t5 - 1.5
-            ds_test = waveforms[k].stats.sampling_rate
-            id_test = np.searchsorted(waveforms[n].times(
-                                      reftime=waveforms[n].stats.starttime - waveforms[n].stats.sac.b), 
-                                      t5_test, side="left")
-            test = waveforms[n].data[id_test:id_test+config['Win']]
-            CorrelationCoefficient, tshift = crs.get_correlation_coefficient(master, test, 1/ds_master)
-            
-            if CorrelationCoefficient >= config['Threshold']:
-                kevnm_test = waveforms[n].stats.sac.kevnm.rstrip()
-                coherence = crs.coherency(master, test, config['low_f'], config['high_f'], ds_master)
-                datetime_test = datetime.datetime.strftime(waveforms[n].stats.starttime.datetime,'%Y.%j.%H%M%S')
-                outline = {1 : datetime_master, 2 : datetime_test, 3 :
-                        int(round(CorrelationCoefficient*1e4)), 4 : int(round(coherence*1e4)), 
-                        5 : '%08d' % (int(kevnm_master)), 6: '%08d' % (int(kevnm_test))}
-                output_list.append(outline)
-            
-            #print('cc: ', round(CorrelationCoefficient*1e4), ' coh: ', round(coherence*1e4))
-            
-            if plotting:
-                fig, ax = plt.subplots(3,1, figsize=(22, 12))
-                t = np.linspace(0, (config['Win']-1)/ds_master, config['Win'])
-                ax[0].plot(t,master, color='black', linewidth=0.5)
-                ax[1].plot(t,test, color='black', linewidth=0.5)
-                ax[2].plot(t, crs.FFTshift(master/np.max(np.abs(master)),float(tshift*ds_master)), linewidth = 0.4)
-                ax[2].plot(t, crs.FFTshift(master/np.max(np.abs(master)),float(tshift*ds_master)), linewidth = 0.4)
-                fig.savefig('plot_' + str(k) + '_' + str(n) + '.png')
-                plt.close()
+
+    start = time.time()
+    output_list =  processing(waveforms, N)
+    end = time.time(    
+    print('Time elapsed: ', end - start, 's.')
+
     df_final = pd.DataFrame.from_dict(output_list)
     print(df_final.head())
     df_final.to_csv(r'pandas.txt', header=None, index=None, sep=' ')
